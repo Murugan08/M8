a0252d4fe859a14c0463a34193e4ef9390342d68
Murugan Krishnakumari Sankaranarayana Pillai <56655314+Murugan08@users.noreply.github.com>
2019-10-30 16:34:10 -0500

    Update project1.R

--- project1.R
+++ project1.R
@@ -2,6 +2,8 @@
 #Data:10/02/19
 #Progess: Still working..
 
+#Short analysis on crime rates in London
+
 #London Crime data simple analysis
 data1 <- london_crime_by_lsoa
 
@@ -24,56 +26,3 @@ max(newdata1$major_category)
 max(newdata1$minor_category)
 #Wounding/GBH
 ##############################################################
-#Short Analaysis on Time series data
-#Mergeddata -> bitcoin , oil, exchange rate gold, sp
-bitcoinnew <- fread('mergeddata.csv')#Bitcoin+Gold+Oil
-summary(bitcoinnew)
-head(bitcoinnew)
-
-bitcoinnew$date <- as.Date(bitcoinnew$date)
-bitcoinnew$date
-
-ts.plot(bitcoinnew$bitcoin, gpars = list(xlab = "day" , ylab = "bitcoin"))
-ggplot(bitcoinnew, aes(x=date,bitcoin)) + geom_line()
-
-#Linear model between the data variables
-model1<-lm(bitcoin~sp+gold+oil+user,data=bitcoinnew)
-summary(model1)
-coeftest(model1,vcov.=vcovHC)
-model2<-lm(log(bitcoin)~log(sp)+log(gold)+log(oil)+log(user),data=bitcoinnew)
-summary(model2)
-#To find relation between oil,gold prices
-rep.kpss <- function(series,maxdiffs=5,crit.val=0.05) {
-  x <- series
-  diffs <- 0
-  for(i in 1:maxdiffs) {
-    pval <- kpss.test(x,null="Level")$p.value
-    if(pval >= crit.val) 
-      return(c(diffs,0,pval))
-    pval <- kpss.test(x,null="Trend")$p.value
-    if(pval >= crit.val) 
-      return(c(diffs,1,pval))
-    x <- diff(x)
-    diffs <- diffs + 1
-  }
-  return(FALSE)
-}
-
-rep.kpss(bitcoinnew$bitcoin)
-rep.kpss(bitcoinnew$sp)
-rep.kpss(bitcoinnew$gold)
-rep.kpss(bitcoinnew$user)
-rep.kpss(bitcoinnew$oil)
-#gold prices are not related with the bitcoin price 
-#oil prices are not related with the bitcoin price 
-
-model2 <- lm(diff(bitcoin)~diff(sp)+diff(gold)+diff(user)+diff(oil), data = bitcoinnew)
-summary(model2)
-coeftest(model2,vcov.=vcovHC)
-#gold prices are not related with the bitcoin price 
-#oil prices are not related with the bitcoin price 
-#euro prices are not related with the bitcoin price
-#it seems that relationship exist between bitcoin and sp
-
-
-
